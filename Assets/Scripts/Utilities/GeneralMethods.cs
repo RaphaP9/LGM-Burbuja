d479e56c7e0efdd3ad804c76e716709769f62207
Raphael <115659111+RaphaP9@users.noreply.github.com>
2025-03-17 13:54:09 -0500

    WebGL Stuff

--- Assets/Scripts/Utilities/GeneralMethods.cs
+++ Assets/Scripts/Utilities/GeneralMethods.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public static class GeneralMethods
 {
-    public static Vector2 TransformPositionVector2(Transform transform) => new Vector2(transform.position.x, transform.position.y);
+    #region Vectors
     public static Vector2 SupressZComponent(Vector3 vector3) => new Vector2(vector3.x, vector3.y);
+    #endregion+
+
+    #region Transforms
+    public static Vector2 TransformPositionVector2(Transform transform) => new Vector2(transform.position.x, transform.position.y);
+
+    public static List<Transform> GetTransformsByColliders(Collider2D[] colliders)
+    {
+        List<Transform> transforms = new List<Transform>();
+
+        foreach (Collider2D collider in colliders)
+        {
+            Transform transform = GetTransformByCollider(collider);
+
+            transforms.Add(transform);
+        }
+
+        return transforms;
+    }
+
+    public static Transform GetTransformByCollider(Collider2D collider) => collider.transform;
+
+    #endregion
 }
